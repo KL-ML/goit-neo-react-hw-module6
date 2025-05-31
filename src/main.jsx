4b293d1d331429901e3ecd569ab72b0a593254c9
KL-ML <knataly0513@gmail.com>
2025-05-31 22:27:26 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import App from './components/App';
-import {store} from './redux/store';
+import {persistor, store} from './redux/store';
 import './index.css';
 import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </BrowserRouter>
+      </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
