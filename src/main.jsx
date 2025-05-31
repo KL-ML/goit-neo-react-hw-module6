c4c3d3d1f9743a6447260f00bceed6ee2c52bebd
KL-ML <knataly0513@gmail.com>
2025-05-31 20:56:04 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import App from './components/App';
+import {store} from './redux/store';
 import './index.css';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
